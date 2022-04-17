12618ba9f47b5be6cfd699e4196211d6f954944f
Adrian Hervas <adrian.hervasmartinez@gmail.com>
2022-04-17 11:18:35 +0200

    Layout login component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { SigninComponent } from './components/signin/signin.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, SigninComponent],
   exports: [SigninComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, AppRoutingModule],
 })
 export class AuthModule {}
